706d42cf0c379e49dddc254d6b5c3b158aae50af
Arjun Mehta <arjun@sighten.io>
2016-12-14 16:33:28 -0800

    add p control methods

--- Python/leap.py
+++ Python/leap.py
@@ -1,10 +1,24 @@
 from constants import (
     MIN_THROTTLE,
-    ALPHA
+    ALPHA,
+    ROLL_ZERO,
+    PITCH_ZERO,
+    YAW_ZERO,
+    ROLL_EXTREME,
+    PITCH_EXTREME,
+    YAW_EXTREME,
+    ROLL_MIN_DRONE,
+    ROLL_MAX_DRONE,
+    PITCH_MIN_DRONE,
+    PITCH_MAX_DRONE,
+    YAW_MIN_DRONE,
+    YAW_MAX_DRONE,
+    KP
 )
 from lib.unix import Leap
 import time
 import threading
+from numpy import clip
 
 
 class LeapMotion(object):
@@ -31,6 +45,31 @@ class LeapMotion(object):
     def update(self, height, roll, pitch, yaw):
         self.height = height
         self.update_roll_pitch_yaw(roll, pitch, yaw)
+
+    def update_p_control_throttle(self, throttle, throttle_drone):
+        throttle_error = clip(throttle, -THROTTLE_EXTREME, THROTTLE_EXTREME) - throttle_drone
+        throttle = clip(THROTTLE_ZERO - KP * int(throttle_error), THROTTLE_MIN_DRONE, THROTTLE_MAX_DRONE)
+        self.height = throttle
+
+    def update_p_control_roll(self, roll, roll_drone):
+        roll_error = clip(roll, -ROLL_EXTREME, ROLL_EXTREME) - roll_drone
+        roll = clip(ROLL_ZERO - KP * int(roll_error), ROLL_MIN_DRONE, ROLL_MAX_DRONE)
+        self.roll = roll
+
+    def update_p_control_pitch(self, pitch, pitch_drone):
+        pitch_error = clip(pitch, -PITCH_EXTREME, PITCH_EXTREME) - pitch_drone
+        pitch = clip(PITCH_ZERO - KP * int(pitch_error), PITCH_MIN_DRONE, PITCH_MAX_DRONE)
+        self.pitch = pitch
+
+    def update_p_control_yaw(self, yaw, yaw_drone):
+        yaw_error = clip(yaw, -PITCH_EXTREME, PITCH_EXTREME) - yaw_drone
+        yaw = clip(YAW_ZERO - KP * int(yaw_error), YAW_MIN_DRONE, YAW_MAX_DRONE)
+        self.yaw = yaw
+
+    def update_p_control_roll_pitch_yaw(self, roll, roll_drone, pitch, pitch_drone, yaw, yaw_drone):
+        self.update_p_control_roll(roll, roll_drone)
+        self.update_p_control_pitch(pitch, pitch_drone)
+        self.update_p_control_yaw(yaw, yaw_drone)
 
     def get(self):
         return [self.height, self.roll, self.pitch, self.yaw]
